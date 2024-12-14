3febbb9fe5d5bcaf8283d0e5ecb784cded48b304
PlayerR9 <riateapad@gmail.com>
2024-12-14 20:18:34 +0100

    update

--- OLD/common/type.go
+++
@@ -1,8 +0,0 @@
-package common
-
-import "github.com/PlayerR9/mygo-lib/OLD/mem"
-
-// Typer is an interface that all types in this library must implement.
-type Typer interface {
-	mem.Freeable
-}
--- fault/helpers.go
+++
@@ -1,6 +0,0 @@
-package fault
-
-const (
-	// DefaultErrorMessage is the default error message for a fault.
-	DefaultErrorMessage string = "something went wrong"
-)
